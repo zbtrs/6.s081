a044f08b192dd2206d018731f0fe68c1e0371788
zbtrs <1339623011@qq.com>
2022-12-21 15:16:48 +0800

    finish sleep

---
+++ user/sleep.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+    int time;
+    if (argc != 2) {
+        fprintf(2, "Usage: sleep...\n");
+        exit(1);
+    }
+    time = atoi(argv[1]);
+    sleep(time);
+
+    exit(0);
+} 
