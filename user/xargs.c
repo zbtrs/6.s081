2ba3de24c8f6b6ae4ef27a9172c133c276d1bb22
zbtrs <1339623011@qq.com>
2022-12-27 12:18:47 +0800

    finish lab1

---
+++ user/xargs.c
@@ -0,0 +1,38 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/param.h"
+#include "user/user.h"
+
+int main(int argc,char *argv[]) {
+
+    char buf[512];
+    int len = read(0,buf,512),cur = 0;
+    char *childargv[MAXARG];
+    for (int i = 0; i + 1 < argc; i++) {
+        childargv[i] = argv[i + 1];
+    }
+
+    while (cur < len) {
+        int ncur = cur;
+        while (ncur < len && buf[ncur] != '\n') {
+            ncur++;
+        }
+        char newargv[512], *p;
+        memset(newargv,0,sizeof(newargv));
+        p = newargv;
+        for (int i = cur; i < ncur; i++) {
+            *p++ = buf[i];
+        }
+        cur = ncur + 1;
+        childargv[argc - 1] = newargv;
+        childargv[argc] = 0;
+        if (fork() == 0) {
+            exec(childargv[0],childargv);
+            printf("cannot exec %s\n",childargv[0]);
+            exit(1);
+        }
+        wait(0);
+    }
+
+    exit(0);
+}
