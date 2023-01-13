640335f11c184c64e88ddcc91f7d85eea39d9779
zbtrs <1339623011@qq.com>
2023-01-13 17:17:48 +0800

    modify vm.c:uvmcopy when using fork

--- kernel/vm.c
+++ kernel/vm.c
@@ -312,7 +312,7 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
   uint64 pa, i;
   uint flags;
   char *mem;
-
+  /*
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
       panic("uvmcopy: pte should exist");
@@ -329,6 +329,21 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
     }
   }
   return 0;
+  */
+  for (i = 0; i < sz; i += PGSIZE) {
+    if ((pte = walk(old,i,0)) == 0) {
+      panic("uvmcopy: pte should exist");
+    }
+    if ((*pte & PTE_V) == 0) {
+      panic("uvmcopy: page not present");
+    }
+    pa = PTE2PA(*pte);
+    *pte = (*pte) & (~PTE_W);
+    flags = PTE_FLAGS(*pte);
+    if (mappages(new,i,PGSIZE,pa,flags) != 0) {
+      goto err;
+    }
+  }
 
  err:
   uvmunmap(new, 0, i / PGSIZE, 1);
