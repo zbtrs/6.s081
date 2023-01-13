30c17c1a33098fcc3053693cc031402275e82c38
zbtrs <1339623011@qq.com>
2023-01-13 17:39:33 +0800

    modify usertrap

--- kernel/trap.c
+++ kernel/trap.c
@@ -68,7 +68,14 @@ usertrap(void)
   } else if((which_dev = devintr()) != 0){
     // ok
   } else if (r_scause() == 12 || r_scause() == 13 || r_scause() == 15) {
-    
+    if (p->killed) {
+      exit(-1);
+    }
+    struct proc* p = myproc();
+    if (cowhandler(p->pagetable,r_stval()) != 0) {
+      p->killed = 1;
+    }
+
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
