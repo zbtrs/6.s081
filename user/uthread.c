c2b6ca70bc202c2aeb99eb574acdbf687ab509dc
zbtrs <1339623011@qq.com>
2023-01-16 16:34:10 +0800

    task1 bug: always run thread_c

--- user/uthread.c
+++ user/uthread.c
@@ -10,11 +10,29 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct tcontext {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct tcontext context;     // context
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -59,6 +77,7 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
+    thread_switch((uint64)&t->context,(uint64)&next_thread->context);
     /* YOUR CODE HERE
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
@@ -77,6 +96,9 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  //memset(t->stack,0,sizeof(t->stack));
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)t->stack;
 }
 
 void 
